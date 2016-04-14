a4ff9473ee5bc60b27447da75601fe7bfdde7ac7
E. Andrew Eccleston <eandreweccleston@Es-MacBook-Pro.local>
2016-04-14 11:33:36 -0500

    everything working

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# heroku_example_server
+# Old Prime Academy example went back into it to cleaning it up
+## hard and pro-mode implemented
 
-Don't forget to run 'npm install' in your console.
+You are getting some information back from the server. You will need to use that information and append it to the DOM. You will need to display each employees picture, name, location, and title.
+
+That's it!
+
+Hard Mode
+Ok, now that you have that information on the DOM, use Bootstrap to organize it all and put each person into a container that has a well class. That way, the display shows each person in a visually organized way. Make sure to also include a little spacing in between each of the people.
+
+Pro Mode
+Instead of the AJAX call happening at document ready, bind it to a button click. So when the experience loads, all the user will see is a button. Once the button is clicked, the server gets the info and sends it back down the client. Once that is successfully received, run the appendDom method. Finally, include a delete button on each person that deletes them from the DOM.
+
--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,30 +1,71 @@
-$(document).ready(function(){
-    $.ajax({
-        type : "GET",
-        url  : "/data",
-        success : function(data) {
-            appendDom(data);
-        }
-    });
+var callLimit = 0;
 
-    $("button").on("click", function(){
-        console.log(data);
-    });
-
+// functions
     function appendDom(data) {
-
-
-
-        for(var i = 0; i < data.people.length; i++) {
-            $(".employeeContainer").append("<div class='person'></div>")
-            $(".person").last().append("<img src='" + data.people[i].imageURL + "'></img>");
-            $(".person").last().append("<p>" + data.people[i].name + "</p>");
-            $(".person").last().append("<p>" + data.people[i].location + "</p>");
-            $(".person").last().append("<p>" + data.people[i].position + "</p>");
-        }
+        
+      for (var i = 0; i < data.people.length; i++){
+        $('.employeeContainer').append( '<div class ="person row well">'+
+                    '<div class = "employee">'+
+                    '<div class = "col-md-2">'+
+                        '<img src = "' + data.people[i].imageURL +'"></img>'+
+                    '</div>'+
+                    '<div class="col-md-3">'+
+                        '<h5>Name: '+data.people[i].name+'</h5>'+
+                        '<h5>Position: '+data.people[i].position+'</h5>'+
+                        '<h5>Location: '+data.people[i].location+'</h5>'+
+                        '<button class = "remove btn btn-info">Remove</button>'+
+                    '</div>'+
+                    '</div>'+
+                '</div>'); 
+         } 
     }
 
+function remove (self) {
+    var item = $(self).parent().parent().parent();    
+    $(item).remove();
+}
 
+function call () {
+  
+  if (callLimit < 1) {  
+   
+    $.ajax({
+        type: "GET",
+        url : "/data",
+        success: function (data) {
+           appendDom(data); 
+           callLimit++;
+        }
+    });
+  
+  } else {
+      return;
+  }
+}
+
+
+$(document).ready(function(){
+    // $.ajax({
+    //     type : "GET",
+    //     url  : "/data",
+    //     success : function(data) {
+    //         appendDom(data);
+            
+    //     }
+    // });
+
+
+//listeners
+$('.employeeContainer').on('click', '.remove', function (){
+    var self = this;
+    remove(self);
+});
+
+$('.jumbotron').on('click', '#gen', function () {
+   call(); 
+});
+
+//end jquery
 });
 
 
